01ed3a3e7ac333c01ae088b984808cac863562e6
Sean McGivern <sean@mcgivern.me.uk>
2013-04-06 12:37:11 +0200

    Move templates to own directory

--- late_junction_playlists.rb
+++ late_junction_playlists.rb
@@ -2,48 +2,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'schema'
 
+set :views, 'view'
+
 get '/' do
   @episodes = Episode.all(:order => [:date.desc])
 
-  erb <<TEMPLATE
-<html>
-<title>Late Junction playlists</title>
-<body>
-<ol>
-<% @episodes.each do |episode| %>
-  <li>
-    <a href="/<%= episode.id %>/"><%= episode.id %> - <%= episode.date %></a>:
-    <%= episode.playlist_tracks.length %>
-  </li>
-<% end %>
-</ol>
-</body>
-</html>
-TEMPLATE
+  erb :episode_list
 end
 
 get '/:episode/' do
   @episode = Episode.get(params['episode'])
 
-  erb <<TEMPLATE
-<html>
-<title><%= @episode.date %></title>
-<body>
-<h2>
-  <a href="<%= @episode.uri %>"><%= @episode.id %> - <%= @episode.date %></a>
-</h2>
-<ol>
-<% @episode.playlist_tracks.each do |playlist_track| %>
-  <li>
-    <code>
-      <%= playlist_track.track.name %> -
-      <%= playlist_track.artists.map {|a| a.name }.join(', ') %>
-    </code>
-  </li>
-<% end %>
-</ol>
-</body>
-</html>
-TEMPLATE
-
+  erb :episode_page
 end
