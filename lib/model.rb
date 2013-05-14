7af88a6638a95c2a00a1f1f9159e61103ac592ce
Sean McGivern <sean@mcgivern.me.uk>
2013-05-14 22:49:43 +0200

    Move model-related methods to lib/model

---
+++ lib/model.rb
@@ -0,0 +1,22 @@
+require 'sequel'
+
+Sequel::Model.plugin(:schema)
+
+def model_constant(s)
+  Class.new.extend(Sequel::Inflections).instance_eval do
+    constantize(singularize(camelize(s)))
+  end
+end
+
+class Sequel::Model
+  def rename(new_name)
+    klass = self.class
+    existing = klass.where(:name => new_name).all
+
+    if existing.empty?
+      update(:name => new_name)
+    else
+      playlist_tracks_dataset.update(klass.table_name.to_sym => existing[0].id)
+    end
+  end
+end
--- schema.rb
+++ schema.rb
@@ -1,7 +1,5 @@
-require 'sequel'
 require 'logger'
-
-Sequel::Model.plugin(:schema)
+require 'lib/model'
 
 def Database(log_file=nil, database_file='tmp/late_junction.db', level=:debug)
   log_to = (log_file ? "tmp/log/#{log_file}" : $stdout)
@@ -13,23 +11,4 @@ def Database(log_file=nil, database_file='tmp/late_junction.db', level=:debug)
   Dir['model/*.rb'].each {|m| require m}
 
   db
-end
-
-def model_constant(s)
-  Class.new.extend(Sequel::Inflections).instance_eval do
-    constantize(singularize(camelize(s)))
-  end
-end
-
-class Sequel::Model
-  def rename(new_name)
-    klass = self.class
-    existing = klass.where(:name => new_name).all
-
-    if existing.empty?
-      update(:name => new_name)
-    else
-      playlist_tracks_dataset.update(klass.table_name.to_sym => existing[0].id)
-    end
-  end
 end
