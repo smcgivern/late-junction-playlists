fbc5619734a49280cd7e34469c062047eaa1fe11
Sean McGivern <sean@mcgivern.me.uk>
2013-06-16 12:54:51 +0200

    Eager load associations graph for playlist items

--- model/track.rb
+++ model/track.rb
@@ -1,6 +1,7 @@
 class Track < Sequel::Model
   include Renameable
-  one_to_many :playlist_tracks
+  one_to_many :playlist_tracks, :eager => [:album, :artists, :composer,
+                                           :episode, :track]
 
   set_schema do
     primary_key :id
