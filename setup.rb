54f4ef379e7102452cd356ba3ab33399c2a08bd9
Sean McGivern <sean@mcgivern.me.uk>
2013-06-16 13:11:56 +0200

    Add settings loading from settings.json

--- setup.rb
+++ setup.rb
@@ -5,6 +5,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'schema'
 
+unless defined? SETTINGS
+  SETTINGS = JSON.parse(open('settings.json').read)
+end
+
+if SETTINGS['sinatra_log']
+  log_file = File.open(SETTINGS['sinatra_log'], 'a')
+
+  $stdout.reopen(log_file)
+  $stderr.reopen(log_file)
+end
+
 DB = Database('production.log')
 
 set :haml, {:format => :html5}
